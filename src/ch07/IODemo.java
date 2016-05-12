9cf22a3337990c8d1bcf537e71922d1a3cfce01b
liuyang <3451378@qq.com>
2016-05-12 10:42:29 +0800

    modified

--- src/ch07/IODemo.java
+++ src/ch07/IODemo.java
@@ -2,6 +2,7 @@ package ch07;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Date;
 
 public class IODemo {
@@ -10,123 +11,40 @@ public class IODemo {
 
     public static PrintStream printStream;
 
-    public static void encodeingConverter(InputStream inputStream, String inEncoding, OutputStream outputStream, String outEncoding) throws IOException {
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, inEncoding);
-
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, outEncoding);
-
-        int count = 0;
-        int ch;
-        while ((ch = inputStreamReader.read()) != -1) {
-            outputStreamWriter.write(ch);
-            count++;
-        }
-        inputStreamReader.close();
-        outputStreamWriter.close();
-        inputStream.close();
-        outputStream.close();
-        printStream.printf("encodeingConverter:%d characters copyed from %s to %s.\n", count, inEncoding, outEncoding);
-    }
-
-    public static ByteArrayOutputStream getBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int count = 0;
-        byte[] byteArray = new byte[ARRAY_SIZE];
-        int len;
-        while ((len = inputStream.read(byteArray)) != -1) {
-            byteArrayOutputStream.write(byteArray, 0, len);
-            count += len;
-        }
-        inputStream.close();
-        byteArrayOutputStream.close();
-        printStream.printf("getBytes:%d bytes getted, using byte[%d] array.\n", count, ARRAY_SIZE);
-
-        return byteArrayOutputStream;
-    }
-
-    public static void copyStream(InputStream inputStream, OutputStream outputStream) throws IOException {
-        int count = 0;
-        int ch;
-        while ((ch = inputStream.read()) != -1) {
-            outputStream.write(ch);
-            count++;
-        }
-        inputStream.close();
-        outputStream.close();
-        printStream.printf("copyStream:%d bytes copyed.\n", count);
-    }
-
-    public static void copyStreamByByteArray(InputStream inputStream, OutputStream outputStream) throws IOException {
-        int count = 0;
-        byte[] byteArray = new byte[ARRAY_SIZE];
-        int len;
-        while ((len = inputStream.read(byteArray)) != -1) {
-            outputStream.write(byteArray, 0, len);
-            count += len;
-        }
-        inputStream.close();
-        outputStream.close();
-        printStream.printf("copyStreamByByteArray:%d bytes copyed, using byte[%d] array.\n", count, ARRAY_SIZE);
-    }
-
-    public static void copyFile(String from, String to) throws IOException {
-        InputStream inputStream = new FileInputStream(from);
-        OutputStream outputStream = new FileOutputStream(to);
-
-        int count = 0;
-        int ch;
-        while ((ch = inputStream.read()) != -1) {
-            outputStream.write(ch);
-            count++;
-        }
-
-        inputStream.close();
-        outputStream.close();
-        printStream.printf("copyFile:%d bytes copyed.\n", count);
-    }
-
     public static void main(String[] args) throws IOException {
-
 //        printStream = new PrintStream("IODemo.out");
-
         printStream = System.out;
 
-        String dir = "/Users/liuyang/Downloads/";
         String from = "test.txt";
         String to = "after-copyied.txt";
-
 
         copyFile(from, to);
 
         copyStream(new FileInputStream(from), new FileOutputStream(to));
-
-        copyStreamByByteArray(new FileInputStream(from), new FileOutputStream(to));
+        printStream.printf("Copy file from \"%s\" to \"%s\" finished.\n", from, to);
 
         ByteArrayOutputStream byteArrayOutputStream = getBytes(new FileInputStream(from));
 
         String fileContent = byteArrayOutputStream.toString();
-
         printStream.println("fileContent is as follows:\n" + fileContent);
         printStream.println("=== end of file ===\n");
 
         String urlStr = "http://www.zzu.edu.cn";
-
         URL url = new URL(urlStr);
-        copyStream(url.openStream(), new FileOutputStream("zzu-homepage.html"));
-//        copyStream(url.openStream(), System.out);
-
+//        copyStream(url.openStream(), new FileOutputStream("zzu-homepage.html"));
+        copyStream(url.openStream(), printStream);
 
         urlStr = "http://www.zzu.edu.cn/images/logo.png";
         url = new URL(urlStr);
-
         copyStream(url.openStream(), new FileOutputStream("zzulogo.png"));
-
         Runtime.getRuntime().exec("open zzulogo.png");
 
+        url = new URL("http://news.zzu.edu.cn");
+//        copyStream(url.openStream(), printStream);
+//        copyCharacterBasedStream(new InputStreamReader(url.openStream(), "gb2312"), new PrintWriter(printStream));
+        copyStreamWithEncoding(url.openStream(), "gb2312", printStream, "utf-8");
+        
         System.exit(0);
-
-        printStream.printf("Copy file from \"%s\" to \"%s\" finished.\n", from, to);
 
         encodeingConverter(new FileInputStream(from), "utf-8", new FileOutputStream(to), "GBK");
 
@@ -154,4 +72,151 @@ public class IODemo {
 
         printStream.printf("Copy file from \"%s\" to \"%s\" finished.\n", from, to);
     }
+
+
+    public static void encodeingConverter(InputStream inputStream, String inEncoding, OutputStream outputStream, String outEncoding) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, inEncoding);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, outEncoding);
+
+        int count = 0;
+        int ch;
+        while ((ch = inputStreamReader.read()) != -1) {
+            outputStreamWriter.write(ch);
+            count++;
+        }
+        inputStreamReader.close();
+        outputStreamWriter.close();
+        inputStream.close();
+        outputStream.close();
+        printStream.printf("encodeingConverter:%d characters copyed from %s to %s.\n", count, inEncoding, outEncoding);
+    }
+
+    public static ByteArrayOutputStream getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int count = 0;
+        byte[] byteArray = new byte[ARRAY_SIZE];
+        int len;
+        while ((len = inputStream.read(byteArray)) != -1) {
+            byteArrayOutputStream.write(byteArray, 0, len);
+            count += len;
+        }
+        inputStream.close();
+        byteArrayOutputStream.close();
+        printStream.printf("getBytes:%d bytes got, using byte[%d] array.\n", count, ARRAY_SIZE);
+
+        return byteArrayOutputStream;
+    }
+
+    public static void stringAndBytesConversion() throws UnsupportedEncodingException {
+        String abc = "abc";
+        byte[] abcBytes = abc.getBytes();
+        printStream.println(Arrays.toString(abcBytes));
+
+        abc = new String(abcBytes, "utf-8");
+        printStream.println(abc);
+    }
+    // Reader Writer
+
+
+    public static void copyStreamWithEncoding(InputStream in, String inEncoding, OutputStream out, String outEncoding) throws IOException {
+        int count = 0;
+        char[] buff = new char[ARRAY_SIZE];
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, inEncoding));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(out, outEncoding));
+
+        int len;
+        while ((len = bufferedReader.read(buff)) != -1) {
+            bufferedWriter.write(buff, 0, len);
+            count += len;
+        }
+        bufferedReader.close();
+        printStream.printf("\nCopyied %d characters with BufferedReader/Writer from %s to %s encoding conversion.\n", count, inEncoding, outEncoding);
+        bufferedWriter.close();
+    }
+
+
+    public static void copyStreamWithBuffer(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int count = 0;
+        byte[] buff = new byte[ARRAY_SIZE];
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+
+        int len;
+        while ((len = bufferedInputStream.read(buff)) != -1) {
+            bufferedOutputStream.write(buff, 0, len);
+            count += len;
+        }
+        bufferedInputStream.close();
+        bufferedOutputStream.close();
+        inputStream.close();
+        outputStream.close();
+        printStream.printf("Copyied %d bytes with BufferedInput/OuputStream.\n", count);
+    }
+
+
+    /**
+     * Please use copyStreamWithBuffer for performance reason.
+     *
+     * @author zzu ie
+     * @see #copyStreamWithBuffer(InputStream, OutputStream)
+     * @since JDK1.7
+     * @deprecated This method is replaced by  <code>copyStreamWithBuffer</code>.
+     */
+    @Deprecated
+    public static void copyStream(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int count = 0;
+        byte[] buff = new byte[ARRAY_SIZE];
+
+        int len;
+        while ((len = inputStream.read(buff)) != -1) {
+            outputStream.write(buff, 0, len);
+            count += len;
+        }
+        inputStream.close();
+        outputStream.flush();
+        printStream.printf("Copyied %d bytes.\n", count);
+        outputStream.close();
+    }
+
+    public static void copyCharacterBasedStream(Reader reader, Writer writer) throws IOException {
+        int count = 0;
+        char[] buff = new char[ARRAY_SIZE];
+
+        int len;
+        while ((len = reader.read(buff)) != -1) {
+            writer.write(buff, 0, len);
+            count += len;
+        }
+        reader.close();
+        writer.flush();
+        printStream.printf("Copyied %d characters.\n", count);
+        writer.close();
+    }
+
+    /**
+     * Copy from input stream into output stream byte by byte.
+     * 按字节方式从输入流拷贝数据到输出流中.
+     *
+     * @author zzu ie
+     * @see #copyFileWithBuffer(String, String)
+     * @since JDK1.7
+     * @deprecated This method is replaced by  <code>copyFileWithBuffer</code>.
+     */
+    @Deprecated
+    private static void copyFile(String from, String to) throws IOException {
+        Date d1 = new Date();
+        copyStream(new FileInputStream(from), new FileOutputStream(to));
+        Date d2 = new Date();
+        printStream.printf("Copy file %s to %s used time is %d  (ms) successfully.\n", from, to, (d2.getTime() - d1.getTime()));
+    }
+
+    private static void copyFileWithBuffer(String from, String to) throws IOException {
+        Date d1 = new Date();
+        copyStreamWithBuffer(new FileInputStream(from), new FileOutputStream(to));
+        Date d2 = new Date();
+        printStream.printf("Copy file %s to %s used time is %d  (ms) with buffered stream successfully.\n", from, to, (d2.getTime() - d1.getTime()));
+    }
+
 }
