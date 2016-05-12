ae75d2ebf0a7aca5bea0e37320196ba537064b26
liuyang <3451378@qq.com>
2016-05-12 11:06:09 +0800

    modified

--- src/ch07/IODemo.java
+++ src/ch07/IODemo.java
@@ -6,9 +6,7 @@ import java.util.Arrays;
 import java.util.Date;
 
 public class IODemo {
-
     public static final int ARRAY_SIZE = 128;
-
     public static PrintStream printStream;
 
     public static void main(String[] args) throws IOException {
