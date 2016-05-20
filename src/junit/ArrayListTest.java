ad78835be66e41c0d8bc96e030867783979d2a35
liuyang <3451378@qq.com>
2016-05-20 19:13:55 +0800

    modified

---
+++ src/junit/ArrayListTest.java
@@ -0,0 +1,38 @@
+package junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ArrayListTest {
+    private ArrayList<String> list = new ArrayList<String>();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testInsertion() {
+        list.add("Beijing");
+        assertEquals("Beijing", list.get(0));
+        list.add("Shanghai");
+        list.add("Hongkong");
+        assertEquals("Hongkong", list.get(list.size() - 1));
+    }
+
+    @Test
+    public void testDeletion() {
+        list.clear();
+        assertTrue(list.isEmpty());
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.remove("B");
+        assertEquals(2, list.size());
+    }
+}
---
+++ src/junit/LoanTest.java
@@ -0,0 +1,47 @@
+package junit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LoanTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testPaymentMethods() {
+        double annualInterestRate = 2.5;
+        int numberOfYears = 5;
+        double loanAmount = 1000;
+        Loan loan = new Loan(annualInterestRate, numberOfYears,
+                loanAmount);
+        assertTrue(loan.getMonthlyPayment() ==
+                getMonthlyPayment(annualInterestRate, numberOfYears,
+                        loanAmount));
+        assertTrue(loan.getTotalPayment() ==
+                getTotalPayment(annualInterestRate, numberOfYears,
+                        loanAmount));
+    }
+
+    /**
+     * Find monthly payment
+     */
+    private double getMonthlyPayment(double annualInterestRate,
+                                     int numberOfYears, double loanAmount) {
+        double monthlyInterestRate = annualInterestRate / 1200;
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1 -
+                (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+        return monthlyPayment;
+    }
+
+    /**
+     * Find total payment
+     */
+    public double getTotalPayment(double annualInterestRate,
+                                  int numberOfYears, double loanAmount) {
+        return getMonthlyPayment(annualInterestRate, numberOfYears,
+                loanAmount) * numberOfYears * 12;
+    }
+}
